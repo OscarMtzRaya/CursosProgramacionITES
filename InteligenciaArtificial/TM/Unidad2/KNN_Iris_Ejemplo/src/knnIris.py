5be174cafb0bcbbe50159109c70166dbc65403a6
OscarMtzRaya <ozcarr753 @gmail.com>
2019-10-21 00:14:28 -0600

    Original configuration added

--- InteligenciaArtificial/TM/Unidad2/KNN_Iris_Ejemplo/src/knnIris.py
+++ InteligenciaArtificial/TM/Unidad2/KNN_Iris_Ejemplo/src/knnIris.py
@@ -1,61 +1,31 @@
 import numpy as np
 from sklearn import datasets
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score, confusion_matrix
-
-# Loading dataset Iris
 iris = datasets.load_iris()
 iris_X = iris.data
 iris_Y = iris.target
-
-dataTrain = [[7.4, 2.8, 6.1, 1.9],
-             [7.9, 3.8, 6.4, 2],
-             [6.4, 2.8, 5.6, 2.2],
-             [5.7, 3, 4.2, 1.2],
-             [5.7, 2.9, 4.2, 1.3],
-             [6.2, 2.9, 4.3, 1.3],
-             [5.1, 2.5, 3, 1.1],
-             [5.4, 3.9, 1.7, 0.4],
-             [4.6, 3.4, 1.4, 0.3],
-             [5, 3.4, 1.5, 0.2]]
-arrayDataTrain = np.array(dataTrain)
-
-targetTrain = [2, 2, 2, 1, 1, 1, 1, 0, 0, 0]
-arrayTargetTrain = np.array(targetTrain)
-
-dataTest = [
-        [5.1, 3.8, 1.5, 0.3],
-        [5.4, 3.4, 1.7, 0.2],
-        [6.7, 3, 5, 1.7],
-        [6, 2.9, 4.5, 1.5],
-        [6.7, 3, 5.2, 2.3],
-        [6.3, 2.5, 5, 1.9]]
-arrayDataTest = np.array(dataTest)
-
-targetTest = [0, 0, 1, 1, 2, 2]
-arrayTargetTest = np.array(targetTest)
-
-# print(arrayData)
-# print(arrayTarget)
-# print("iris data",iris_X)
-# print("targets",iris_Y)
+print("iris data", iris_X)
+print("targets", iris_Y)
 
 np.random.seed(0)
-indices = np.random.permutation(len(arrayDataTrain))
-iris_X_train = arrayDataTrain
-iris_Y_train = arrayTargetTrain
-iris_X_test = arrayDataTest
-iris_Y_test = arrayTargetTest
+indices = np.random.permutation(len(iris_X))
+iris_X_train = iris_X[indices[:-45]]
+iris_Y_train = iris_Y[indices[:-45]]
+iris_X_test = iris_X[indices[-45:]]
+iris_Y_test = iris_Y[indices[-45:]]
 
 # KNN
-knn = KNeighborsClassifier(n_neighbors=3)
+from sklearn.neighbors import KNeighborsClassifier
+
+knn = KNeighborsClassifier(n_neighbors=1)
 knn.fit(iris_X_train, iris_Y_train)
 
 output = knn.predict(iris_X_test)
 print("output\n", output)
 print("resultados esperados\n", iris_Y_test)
+print("diferencia(output - iris_Y_test)\n", output - iris_Y_test)
 
-# Accuracy score
+from sklearn.metrics import accuracy_score, confusion_matrix
+
 accuracy = accuracy_score(iris_Y_test, output)
 print("Exactitud : %0.1f%% " % (accuracy * 100))
 
